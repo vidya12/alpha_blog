d093c3839592080ca257b00ca30074ad292c08a2
vidya <vidya.bolkaranda@gmail.com>
2016-05-12 09:08:56 +0000

    create new template, action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+  def create
+   # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506183945) do
+ActiveRecord::Schema.define(version: 20160506184738) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text   "description"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
