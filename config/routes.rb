de961c953537c8338f3d4adce187048950228ff0
vidya <vidya.bolkaranda@gmail.com>
2016-07-22 18:35:50 +0000

    Add categoreis model and test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
+
   resources :comments
   resources :comments
   resources :comments
